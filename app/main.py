2eee037a598ab8d3b8a605ed006aef517baa3609
pvoipt <dohyun682@gmail.com>
2022-07-07 19:06:33 +0900

    initial commit

---
+++ app/main.py
@@ -0,0 +1,57 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+
+app = FastAPI()
+
+
+class ProductInfo(BaseModel):
+    product_uuid: str
+    seller_uuid: str
+    product_name: str
+    product_img: List[str]
+    product_hashtag: List[str]
+    product_price: int
+    product_effective: int
+    product_discount: int
+    product_create_date: str
+
+
+class ProductDetail(BaseModel):
+    product_color: str
+    product_size: str
+    size_add_price: int
+    product_stock: bool
+    product_sales: int
+    size_chest: float
+    size_back: float
+    size_leg: float
+    size_margin: float
+
+
+class DogInfo(BaseModel):
+    dog_uuid: str
+    user_uuid: str
+    dog_nickname: str
+    dog_type: str
+    dog_gender: str
+    dog_weight: float
+    dog_size_img: str | None
+    dog_size_chest: float
+    dog_size_back: float
+    dog_size_neck: float
+    dog_size_leg: float
+    dog_create_time: str
+    is_default: bool
+    is_delete: bool
+
+
+class RecommendInput(BaseModel):
+    product_info: ProductInfo
+    product_details: List[ProductDetail]
+    dog_info: DogInfo
+
+
+@app.post("/recommend")
+async def recommend_size(recommend_input: RecommendInput):
+    return
